6ca8d8db0a6228b6121992d804dc73d167f83345
Conor Wilson <conor.wilson@actionrocket.co>
2025-02-26 17:41:56 +0000

    Delete fetch.js

--- debug/fetch.js
+++
@@ -1,7 +0,0 @@
-const url = 'https://files.actionrocket.dev/supercell/2025/connected_content/schema.json';
-
-const response = await fetch(url);
-
-const schema = await response.json();
-
-console.log(schema);
